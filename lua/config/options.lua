034dba085eb40b2f045cd3142a506f8bb7e2f663
Fernando Ollé <fernando.olle@icloud.com>
2025-01-03 11:33:31 -0300

    init repo

---
+++ lua/config/options.lua
@@ -0,0 +1,8 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
+vim.opt.scrolloff = 20
+vim.opt.guicursor = "n-v-i-c:block-Cursor"
+
+-- vim.o.numberwidth = 50
