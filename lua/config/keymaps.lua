e8978d733f9e2b71416f35199577115a4aff4652
Fernando Ollé <fernando.olle@icloud.com>
2025-01-28 02:05:06 -0300

    add deepSeek

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Mapping CMD + Right/Left Arrow in insert mode to jump to line end/start
+vim.keymap.set("i", "<D-Right>", "<Esc>A", { silent = true })
+vim.keymap.set("i", "<D-Left>", "<Esc>0i", { silent = true })
