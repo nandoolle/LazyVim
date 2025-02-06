28bb649847d3eb4be5b1ed6d647bd5b30acf4abe
Fernando Ollé <fernando.olle@icloud.com>
2025-02-06 17:45:52 -0300

    add autoimport on save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local function augroup(name)
+  return vim.api.nvim_create_augroup("autocmd_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = augroup("organise_imports"),
+  pattern = "*.ts",
+  callback = function()
+    vim.lsp.buf.code_action({ apply = true, context = { only = { "source.addMissingImports.ts" }, diagnostics = {} } })
+    vim.lsp.buf.code_action({ apply = true, context = { only = { "source.removeUnused.ts" }, diagnostics = {} } })
+  end,
+})
