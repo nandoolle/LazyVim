7d94a368f4fadb7f765edbf96c0c6aeda49e5f89
Fernando Ollé <fernando.olle@icloud.com>
2025-02-18 03:19:57 -0300

    add rose-pine

---
+++ lua/plugins/retro-theme.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "mistweaverco/retro-theme.nvim",
+    name = "retro-theme",
+    config = function()
+      vim.cmd("colorscheme retro-theme")
+    end,
+
+    -- {
+    --   "LazyVim/LazyVim",
+    --   opts = {
+    --     colorscheme = "retro-theme",
+    --   },
+    -- },
+    --
+    -- opts = {
+    --   transparent = true,
+    --   styles = {
+    --     sidebars = "transparent",
+    --     floats = "transparent",
+    --   },
+    -- },
+  },
+}
