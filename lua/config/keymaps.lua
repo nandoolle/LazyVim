74ef6d1697adf8ddb32a63691196653d5fed6946
Fernando Ollé <fernando.olle@icloud.com>
2025-03-18 07:24:48 -0300

    add oil

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,3 +19,5 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+vim.keymap.set("n", "-", "<cmd>Oil<CR>")
