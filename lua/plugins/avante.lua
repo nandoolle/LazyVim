0aa63c7acc652412fe3f3b12f02f415f30e5c64a
Fernando Ollé <fernando.olle@icloud.com>
2025-05-16 10:32:03 -0300

    add cop-cmp and avante update

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -14,23 +14,32 @@ return {
   lazy = false,
   version = false, -- set this if you want to always pull the latest change
   opts = {
-    provider = "claude", -- The provider used in Aider mode or in the planning phase of Cursor Planning Mode
+    ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+    provider = "claude", -- Recommend using Claude
+    auto_suggestions_provider = "claude", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
     claude = {
       endpoint = "https://api.anthropic.com",
-      model = "claude-3-7-sonnet-20250219",
+      model = "claude-3-5-sonnet-20240620",
       temperature = 0,
+      max_tokens = 4096,
     },
+    -- provider = "claude", -- The provider used in Aider mode or in the planning phase of Cursor Planning Mode
+    -- claude = {
+    --   endpoint = "https://api.anthropic.com",
+    --   model = "claude-3-7-sonnet-20250219",
+    --   temperature = 0,
+    -- },
     -- add any opts here
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
+    "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
     "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
