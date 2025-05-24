0c16feb1cf024ac4d987c5f4304d93147ed5a83e
Fernando Ollé <fernando.olle@icloud.com>
2025-05-24 01:56:52 -0300

    add integration to claude 4

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -5,17 +5,19 @@ return {
   opts = {
     -- add any opts here
     -- for example
-    provider = "internal",
-    vendors = {
-      internal = {
-        __inherited_from = "openai",
-        endpoint = "http://127.0.0.1:8899/v1",
-        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-        temperature = 0,
-        max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-        reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-      },
+    provider = "claude",
+    claude = {
+      endpoint = "https://api.anthropic.com",
+      model = "claude-sonnet-4-20250514",
+      disable_tools = true,
+      temperature = 0,
+      max_tokens = 8192,
+    },
+    behaviour = {
+      auto_suggestions = false,
+      minimize_diff = false,
+      enable_cursor_planning_mode = true,
+      enable_claude_text_editor_tool_mode = true,
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
