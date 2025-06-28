7999e691f043bca4be32e3edf3022f0ddf457f51
Fernando Ollé <fernando.olle@icloud.com>
2025-06-28 01:17:01 -0300

    add things that I dont really remember what they are...

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -17,37 +17,37 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 })
 
 --  custom spell check
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-  group = augroup("auto_spell"),
-  callback = function()
-    -- Avoid running on certain file types or unnamed buffers
-    local filetype = vim.bo.filetype
-    local filename = vim.fn.expand("%:t")
-
-    -- Skip for special buffers, empty buffers, or certain filetypes
-    if
-      filetype == ""
-      or filetype == "NvimTree"
-      or filetype == "TelescopePrompt"
-      or filetype == "fugitive"
-      or filetype == "help"
-      or filename == ""
-    then
-      return
-    end
-
-    -- -- Enable camel case support for code identifiers
-    vim.opt_local.spell = true
-    vim.opt_local.spelloptions = "camel"
-
-    -- Use a visible but not intrusive highlighting
-    vim.api.nvim_set_hl(0, "SpellBad", { undercurl = true, sp = "red", cterm = { underline = true } })
-
-    -- For code files: enable smart spell checking that excludes framework code
-    vim.cmd([[
-      syntax clear
-      syntax spell toplevel
-      syntax spell default
-      ]])
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+--   group = augroup("auto_spell"),
+--   callback = function()
+--     -- Avoid running on certain file types or unnamed buffers
+--     local filetype = vim.bo.filetype
+--     local filename = vim.fn.expand("%:t")
+--
+--     -- Skip for special buffers, empty buffers, or certain filetypes
+--     if
+--       filetype == ""
+--       or filetype == "NvimTree"
+--       or filetype == "TelescopePrompt"
+--       or filetype == "fugitive"
+--       or filetype == "help"
+--       or filename == ""
+--     then
+--       return
+--     end
+--
+--     -- -- Enable camel case support for code identifiers
+--     vim.opt_local.spell = true
+--     vim.opt_local.spelloptions = "camel"
+--
+--     -- Use a visible but not intrusive highlighting
+--     vim.api.nvim_set_hl(0, "SpellBad", { undercurl = true, sp = "red", cterm = { underline = true } })
+--
+--     -- For code files: enable smart spell checking that excludes framework code
+--     vim.cmd([[
+--       syntax clear
+--       syntax spell toplevel
+--       syntax spell default
+--       ]])
+--   end,
+-- })
